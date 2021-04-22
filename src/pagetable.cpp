14ea861c3a15a5918fe9f883b29ba82477dec8c3
Giblit42 <Jone1905@stthomas.edu>
2021-04-22 10:05:23 -0500

    made changes to page table

--- src/pagetable.cpp
+++ src/pagetable.cpp
@@ -31,6 +31,12 @@ void PageTable::addEntry(uint32_t pid, int page_number)
 
     int frame = 0; 
     // Find free frame
+    if(_table[entry] == null)
+    {
+        _table[entry] = frame;
+    }
+    //page replacement
+    
     // TODO: implement this!
     _table[entry] = frame;
 }
@@ -38,18 +44,18 @@ void PageTable::addEntry(uint32_t pid, int page_number)
 int PageTable::getPhysicalAddress(uint32_t pid, uint32_t virtual_address)
 {
     // Convert virtual address to page_number and page_offset
-    // TODO: implement this!
-    int page_number = 0;
-    int page_offset = 0;
+    int page_number = virtual_address / page_size;
+    int page_offset = virtual_address % page_size;
 
     // Combination of pid and page number act as the key to look up frame number
     std::string entry = std::to_string(pid) + "|" + std::to_string(page_number);
     
     // If entry exists, look up frame number and convert virtual to physical address
+    // entry is frame number 
     int address = -1;
     if (_table.count(entry) > 0)
     {
-        // TODO: implement this!
+       address = entry + page_offset;
     }
 
     return address;
@@ -66,6 +72,6 @@ void PageTable::print()
 
     for (i = 0; i < keys.size(); i++)
     {
-        // TODO: print all pages
+        printf("%5u | %d | %d/n", pid, page_number, frame);
     }
 }
